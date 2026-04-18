2e6fe02f1466fb06e7307c139a854d76519870e6
buinta-k <22255z2h72@privaterelay.appleid.com>
2026-04-18 20:40:41 +0300

    daysteps.go

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,44 @@
 package daysteps
 
+import ("strings"
+		"strconv"
+	   "github.com/buinta-k/go1fl-sprint5-final/blob/main/internal/spentenergy/spentenergy.go"
+	   )
+
 type DaySteps struct {
-	// TODO: добавить поля
+	Steps    int
+	Duration time.Duration
+	personaldata.Personal 
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
-	// TODO: реализовать функцию
+	dataString := strings.Split(datastring, ",")
+    if len(dataString) < 2 {
+        return fmt.Errorf("Некорректный формат")
+    }
+
+	ds.Steps, err = strconv.Atoi(dataString[0])
+	if err != nil {
+		return fmt.Errorf("Ошибка обработки данных: %w", err)
+	}
+
+	ds.Duration, err = time.ParseDuration(dataString[1])
+    if err != nil {
+        return fmt.Errorf("Ошибка обработки данных: %w", err)
+    }
+    return nil
+	
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
-	// TODO: реализовать функцию
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	
+    if err != nil {
+		return "", err
+	}
+
+	result := fmt.Sprintf("Количество шагов: %d.\n Дистанция: %d км.\n Вы сожгли %d ккал.\n", steps, distanсe, calories)
+
+	return result, nil
 }
